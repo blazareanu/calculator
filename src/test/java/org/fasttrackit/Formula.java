10d890288a4bbbe6557fc760ceb9054f7b282035
blazareanu <lazareanubianca@gmail.com>
2019-01-14 01:54:20 +0200

    Initial commit

---
+++ src/test/java/org/fasttrackit/Formula.java
@@ -0,0 +1,14 @@
+package org.fasttrackit;
+
+public class Formula {
+    double sum;
+
+
+    public double firstCalc(double x, double y) {
+
+      sum = firstCalc(10,10);
+
+        double calc1 = x + y;
+        return calc1;
+    }
+}
