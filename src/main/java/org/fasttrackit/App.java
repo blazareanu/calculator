f3c63caaf4830937abbe1c44cfd42746f5866ee0
blazareanu <lazareanubianca@gmail.com>
2019-01-14 07:35:06 +0200

    Try1

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,16 +1,17 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.lang.reflect.Field;
+import java.text.Normalizer;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static double main(String[] args ) {
+        Formula calc1 = new Formula();
+
+        double firstFormula = calc1.firstCalc(10, 10);
+        System.out.println("The result will be " + firstFormula);
+        return firstFormula;
+
+        
     }
-
-
-
 }
---
+++ src/main/java/org/fasttrackit/Formula.java
@@ -0,0 +1,20 @@
+package org.fasttrackit;
+
+public class Formula {
+    double sum;
+
+
+
+    public double firstCalc(double x, double y) {
+
+        double calc1 = x + y;
+        return calc1;
+
+    }
+
+    public double secondCalc (double a, double b, double c, double d){
+
+        double calc2= a*b*c*d;
+        return calc2;
+    }
+}
